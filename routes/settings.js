a96d7cae2a33f8a304bb510d9134028630326a32
Yitayal AYENEW <merawiyitu@gmail.com>
2026-03-01 18:55:33 +0300

    Convert EMS to Client-only repository: Moved client files to root and removed server-side components

--- routes/settings.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { changePassword, updateProfile } = require('../controllers/settingController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/change-password', protect, changePassword);
-router.put('/update-profile', protect, updateProfile);
-
-module.exports = router;
